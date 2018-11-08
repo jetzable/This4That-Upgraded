9912dc92fc22d7341f56b17de6491c89f5caddb2
Palmidia Jetzabel Garay Dávila <gjetzable@gmail.com>
2018-11-07 23:51:19 -0600

    Testing login and signUp functions

--- js/signUp.js
+++ js/signUp.js
@@ -1,34 +1,21 @@
 initializeFirebase();
-let db = firebase.firestore();
-let dbSettings = { timestampsInSnapshots: true };
-db.settings(dbSettings);
-
-const storage = firebase.storage();
+const db = firebase.firestore();
+db.settings({
+  timestampsInSnapshots: true
+});
 
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
     // User is signed in.
-    const displayName = user.displayName;
-    const email = user.email;
-    const emailVerified = user.emailVerified;
-    const photoURL = user.photoURL;
-    const isAnonymous = user.isAnonymous;
-    const uid = user.uid;
-    const providerData = user.providerData;
-    // location.href = "dashboard.html";
-    // ...
+    let displayName = user.displayName;
+    let email = user.email;
+    let emailVerified = user.emailVerified;
+    let photoURL = user.photoURL;
+    let isAnonymous = user.isAnonymous;
+    let uid = user.uid;
+    let providerData = user.providerData;
+    location.href = "../views/dashboard.html";
+  } else {
+    // User is signOut.
   }
-});
-
-document.getElementById("signUp-button").addEventListener("click", event => {
-  event.preventDefault();
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("pwd").value;
-  const name = document.getElementById("name").value;
-  const city = document.getElementById("city").value;
-  const photo = document.getElementById("profilePhoto").files[0];
-  const storageRef = firebase.storage().ref("profile_photos/" + photo.name);
-  createUserProfileWithEmail(name, email, city, photo, storageRef);
-
-  newAccount(email, password);
 });
